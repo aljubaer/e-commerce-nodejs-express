bd957a5b30409cb98f206edb6dbbc6c49ed6307a
aljubaer <bsse0812@iit.du.ac.bd>
2021-09-21 12:41:24 +0600

    adds defaults routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
 import 'dotenv/config';
-import App from '@app';
-import AuthRoute from 'modules/auth/auth.route';
-import IndexRoute from 'modules/defaults/index.route';
-import UsersRoute from 'modules/user/users.route';
+import App from './app';
+import AuthRoute from '@modules/auth/auth.route';
+import IndexRoute from '@modules/defaults/index.route';
+import UsersRoute from '@modules/user/users.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
